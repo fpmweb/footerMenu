155e8b3a37d1bc21d506f8d289cccafc0d7391f1
Francesc Puig <fpuig@yaencontre.com>
2016-03-30 12:43:19 +0200

    add bower

---
+++ bower.js
@@ -0,0 +1,26 @@
+{
+name: "footerMenu",
+homepage: "https://github.com/fpmweb/footerMenu",
+authors: [
+"Francesc Puig <francesc.puig@gmail.com>"
+],
+description: "Simple and easy jQuery plugin. Nice slide up toggle footer menu when scroll down.",
+main: "footerMenu.html",
+moduleType: [ ],
+keywords: [
+"footerMenu",
+"menu",
+"footer",
+"message",
+"fpmweb",
+"css3",
+"javascript",
+"css",
+"simple",
+"jquery",
+"plugin",
+"nice",
+"animation"
+],
+license: "MIT"
+}
